88144cf2e98270339c9f4d692b98f877f210d722
Kirandana Arkanalla <88313502+Kirandana@users.noreply.github.com>
2022-03-17 11:24:31 +0700

    Additions

--- quiz-game.py
+++ quiz-game.py
@@ -8,9 +8,32 @@ if question != "yes":
 if question == "yes":
     print("Okay, let's play!")
 
-cpu = input("What does CPU stand for? ")
-if cpu != "central processing unit":
+    # Quiz Starts Here
+
+questionOne = input("What does CPU stand for? ")
+if questionOne != "central processing unit":
     print("Incorrect!")
 
-if cpu == "central processing unit":
+if questionOne == "central processing unit":
     print("Correct, you got one point!")
+
+questionTwo = input("What does CSS stand for? ")
+if questionTwo != "cascading stylesheet":
+    print("Incorrect!")
+
+if questionTwo == "cascading stylesheet":
+    print("Correct!")
+
+questionThree = input("What does HTML stand for? ")
+if questionThree != "hypertext markup language":
+    print("Incorrect!")
+
+if questionThree == "hypertext markup language":
+    print("Correct!")
+
+questionFour = input("What does JS stand for? ")
+if questionFour != "javascript":
+    print("Incorrect!")
+
+if questionFour == "javascript":
+    print("Correct!")
